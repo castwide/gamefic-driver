33b1222dac5233dddeb775ac2cdc94e513f8036d
Fred Snyder <fsnyder@castwide.com>
2023-04-10 06:11:11 -0400

    OpalDriver tests

--- test/OpalDriver.test.ts
+++ test/OpalDriver.test.ts
@@ -1,35 +1,47 @@
 import { OpalDriver } from '../src/OpalDriver';
 
-let opalMock = {
-    gvars: {
-        plot: {
-            $ready: () => { },
-            $update: () => { },
-            $save: () => {
-                return {
-                    $to_json: () => {
-                        return "{}";
-                    }
-                };
+let plot = {
+    $ready: () => { },
+    $update: () => { },
+    $save: () => {
+        return {
+            $to_json: () => {
+                return "{}";
             }
-        },
-        character: {
-            $output: () => {
-                return {
-                    $to_json: () => {
-                        return JSON.stringify({
-                            messages: "<p>the messages</p>",
-                            queue: []
-                        });
-                    }
-                };
-            },
-            $queue: () => {
-                return {
-                    $push: (_input) => { }
-                }
+        };
+    },
+    $make_player_character: () => { return character },
+    $introduce: (_) => {},
+    $players: () => { return {
+        $first: () => { return character }
+    }}
+}
+
+let character = {
+    $output: () => {
+        return {
+            $to_json: () => {
+                return JSON.stringify({
+                    messages: "<p>the messages</p>",
+                    queue: []
+                });
             }
+        };
+    },
+    $queue: () => {
+        return {
+            $push: (_input) => { }
         }
+    }
+}
+
+let opalMock = {
+    gvars: {},
+    Object: {
+        $const_get: (_) => { return {
+            $new: ()=> { return plot },
+            $restore: (_) => { return plot }
+        }}
     }
 }
 
@@ -37,7 +49,7 @@ describe('OpalDriver', () => {
     it('starts a plot', () => {
         let driver = new OpalDriver(opalMock);
         return driver.start().then(result => {
-            let parsed = JSON.parse(opalMock.gvars.character.$output().$to_json());
+            let parsed = JSON.parse(opalMock.gvars['player'].$output().$to_json());
             expect(result).toEqual(parsed);
         });
     });
