5b7ac482c952f39cdb68ab29d37c2bbd508a1a09
Fred Snyder <fsnyder@castwide.com>
2023-04-10 05:58:17 -0400

    OpalDriver maintains plot and player instances

--- src/OpalDriver.ts
+++ src/OpalDriver.ts
@@ -10,22 +10,25 @@ export class OpalDriver extends Driver {
 
 	start() {
 		return new Promise((resolve) => {
+			this.opal.gvars.plot = this.opal.Object.$const_get('Gamefic::Plot').$new();
+			this.opal.gvars.player = this.opal.gvars.plot.$make_player_character();
+			this.opal.gvars.plot.$introduce(this.opal.gvars.player);
 			this.opal.gvars.plot.$ready();
-			var state = this.opal.gvars.character.$output().$to_json();
+			var state = this.opal.gvars.player.$output().$to_json();
 			var result = JSON.parse(state);
 			resolve(result);	
 		});
 	}
 
 	receive(input: string) {
-		this.opal.gvars.character.$queue().$push(input);
+		this.opal.gvars.player.$queue().$push(input);
 		return this.update();
 	}
 
 	update() {
 		this.opal.gvars.plot.$update();
 		this.opal.gvars.plot.$ready();
-		var state = this.opal.gvars.character.$output().$to_json();
+		var state = this.opal.gvars.player.$output().$to_json();
 		var result = JSON.parse(state);
 		this.notify(result);
 		if (result.queue.length > 0) {
@@ -45,9 +48,9 @@ export class OpalDriver extends Driver {
 
 	restore(snapshot: any) {
 		this.opal.gvars.plot = this.opal.Object.$const_get('Gamefic::Plot').$restore(snapshot);
-		this.opal.gvars.character = this.opal.gvars.plot.$players().$first();
+		this.opal.gvars.player = this.opal.gvars.plot.$players().$first();
 		this.opal.gvars.plot.$ready();
-		var state = this.opal.gvars.character.$output().$to_json();
+		var state = this.opal.gvars.player.$output().$to_json();
 		var result = JSON.parse(state);
 		this.notify(result);
 		return new Promise((resolve) => {
